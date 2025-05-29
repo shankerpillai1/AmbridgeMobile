f2cfc9e69261ce0e900a7cabf41cdc47ff47923f
Shanker Pillai <67911778+shankerpillai1@users.noreply.github.com>
2025-05-29 14:31:36 -0400

    Working launch and navigation bar

--- Frontend/AmbridgeMobile/app/_layout.tsx
+++ Frontend/AmbridgeMobile/app/_layout.tsx
@@ -1,29 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Tabs } from 'expo-router';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+export default function Layout() {
+    return (
+        <Tabs>
+            <Tabs.Screen name="home" options={{ title: 'Home' }} />
+            <Tabs.Screen name="tours" options={{ title: 'Tours' }} />
+            <Tabs.Screen name="info" options={{ title: 'Info' }} />
+        </Tabs>
+    );
 }
---
+++ Frontend/AmbridgeMobile/app/info.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function InfoScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Info</Text>
+        </View>
+    );
+}
