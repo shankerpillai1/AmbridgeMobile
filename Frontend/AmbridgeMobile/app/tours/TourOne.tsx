af503106ee69bbe0c855eec4ef4a824f93e14a45
Shanker Pillai <67911778+shankerpillai1@users.noreply.github.com>
2025-07-25 13:05:04 -0400

    color change functionality for markers

--- Frontend/AmbridgeMobile/app/tours/TourOne.tsx
+++ Frontend/AmbridgeMobile/app/tours/TourOne.tsx
@@ -31,9 +31,26 @@ export default function TourOne() {
             }
 
             const markersJs = data.map((loc: Location) => `
-        L.marker([${loc.latitude}, ${loc.longitude}])
+        const marker${loc.id} = L.marker([${loc.latitude}, ${loc.longitude}], {
+          icon: L.icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+          })
+        })
           .addTo(map)
           .on('click', () => {
+            marker${loc.id}.setIcon(L.icon({
+              iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+              shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowSize: [41, 41]
+            }));
             window.ReactNativeWebView.postMessage('${loc.id}');
           });
       `).join('\n');
